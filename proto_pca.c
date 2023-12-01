cc15e7b660cafde3ae1588966bfca367c93d610c
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-11-30 17:24:25 -0800

    mmread_centered

--- proto_pca.c
+++ proto_pca.c
@@ -6,11 +6,13 @@
 #include <time.h>
 #include <math.h>
 #include "distpca.h"
+#include "mmio.h"
 
 int p = 10;
 char *fname = NULL;
 
 int usage(char *argv[]);
+double* mmread_centered(char const *fname, int *n, int *d);
 
 int main(int argc, char *argv[])
 {
@@ -28,6 +30,14 @@ int main(int argc, char *argv[])
         return usage(argv);
     }
 
+    int n, d;
+    double *A;
+
+    A = mmread_centered(argv[optind], &n, &d);
+
+    mmwrite("Acentered.mtx", A, n, d);
+
+    free(A);
     return 0;
 }
 
@@ -37,4 +47,43 @@ int usage(char *argv[])
     fprintf(stderr, "Options: -p INT   number of principal components [%d]\n", p);
     fprintf(stderr, "         -h       help message\n");
     return 1;
+}
+
+double* mmread_centered(char const *fname, int *n, int *d)
+{
+    MM_typecode matcode;
+    int N, D, i, j;
+    double *A;
+    FILE *f;
+
+    f = fopen(fname, "r");
+    mm_read_banner(f, &matcode);
+    assert(mm_is_dense(matcode));
+    mm_read_mtx_array_size(f, &N, &D);
+    A = malloc(N*D*sizeof(double));
+    assert(A != NULL);
+
+    for (j = 0; j < D; ++j)
+    {
+        double *col = &A[j*N];
+        double mean = 0.;
+
+        for (i = 0; i < N; ++i)
+        {
+            fscanf(f, "%lg", &col[i]);
+            mean += col[i];
+        }
+
+        mean /= N;
+
+        for (i = 0; i < N; ++i)
+            col[i] -= mean;
+    }
+
+    fclose(f);
+
+    *n = N;
+    *d = D;
+
+    return A;
 }
