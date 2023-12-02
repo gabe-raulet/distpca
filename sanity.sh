56767d6b0ff46d2a7177bd7735ff6bd8609e4fba
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-12-02 10:52:36 -0800

    move around stuff

--- sanity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -f *.mtx *.diag
-
-echo "./gen_svd -m 256 -n 128 -u -1 -c 100 -d 2 -o sanity"
-./gen_svd -m 256 -n 128 -u -1 -c 100 -d 2 -o sanity
-
-echo "mpirun -np 8 ./dist_svd sanity_A.mtx sanity_S.diag sanity_U.mtx sanity_Vt.mtx 10"
-mpirun -np 8 ./dist_svd sanity_A.mtx sanity_S.diag sanity_U.mtx sanity_Vt.mtx 10
